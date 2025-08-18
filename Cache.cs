cd10950639dd05ab38688fe238f147ae89e66755
Steven Fawcett <steven.fawcett@me.com>
2025-08-18 18:05:11 +0100

    Make it service driven

--- Cache.cs
+++ Cache.cs
@@ -1,28 +1,21 @@
-﻿using BroadcastPluginSDK;
+﻿using System.Diagnostics;
+using BroadcastPluginSDK.abstracts;
 using LocalCachePlugin.Properties;
 using Microsoft.Extensions.Configuration;
-using System.Configuration;
-using System.Diagnostics;
 
 namespace LocalCachePlugin;
 
 public class Cache : BroadcastCacheBase
 {
     private static Dictionary<string, string> _internalCache = [];
-    private static readonly CachePage s_infoPage = new CachePage();
+    private static readonly CachePage s_infoPage = new();
     private static readonly Image s_icon = Resources.green;
 
-    public Cache(IConfiguration configuration) : base(null, s_infoPage, s_icon, "Local Cache", "Local", true, "Simple Cache")
+    public Cache(IConfiguration configuration) : base(configuration, s_infoPage, s_icon, "Local Cache", "Local" ,
+        "Simple Cache")
     {
         _internalCache = new Dictionary<string, string>();
-
-        var config = configuration.GetSection("Local").GetChildren();
-
-        // Fix for CS0021: Use LINQ to find the "Master" configuration section
-        var masterSection = config.FirstOrDefault(section => section.Key == "Master");
-        Master = masterSection != null && bool.TryParse(masterSection.Value, out var masterValue) && masterValue;
     }
-
 
     public override void Clear()
     {
@@ -31,10 +24,7 @@ public class Cache : BroadcastCacheBase
 
     public override void Write(Dictionary<string, string> data)
     {
-        foreach (var kv in data)
-        {
-            _internalCache[kv.Key] = kv.Value;
-        }
+        foreach (var kv in data) _internalCache[kv.Key] = kv.Value;
 
         s_infoPage.redraw(_internalCache);
     }
@@ -46,22 +36,16 @@ public class Cache : BroadcastCacheBase
         return Read(values).ToList();
     }
 
-
     public static IEnumerable<KeyValuePair<string, string>> Read(List<string> values)
     {
-        foreach (var value in values)
-        {
-            yield return ReadValue(value);
-        }
+        foreach (var value in values) yield return ReadValue(value);
     }
 
     public static IEnumerable<KeyValuePair<string, string>> Read()
     {
-        foreach (var kvp in _internalCache)
-        {
-            yield return new KeyValuePair<string, string>(kvp.Key, kvp.Value);
-        }
+        foreach (var kvp in _internalCache) yield return new KeyValuePair<string, string>(kvp.Key, kvp.Value);
     }
+
     public static KeyValuePair<string, string> ReadValue(string value)
     {
         if (_internalCache.TryGetValue(value, out var data))
